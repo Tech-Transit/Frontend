2fb6b5a2602511ff4e66872ad000eda70aa5841a
pratibhasingh47 <pratibhasingh1624@gmail.com>
2025-03-08 12:28:23 +0530

    Map_frontend

--- src/Components/MapComponent.jsx
+++ src/Components/MapComponent.jsx
@@ -1,29 +1,39 @@
-import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, Polyline, useMapEvents } from "react-leaflet";
 import { useState } from "react";
 import "leaflet/dist/leaflet.css";
 
 const MapComponent = () => {
     const [waypoints, setWaypoints] = useState([
-        [51.505, -0.09], // Default starting location (London)
+        [19.0760, 72.8777], // Default starting location (Mumbai)
     ]);
 
-    const handleMapClick = (e) => {
-        const newWaypoint = [e.latlng.lat, e.latlng.lng];
-        setWaypoints([...waypoints, newWaypoint]);
+    // Handle map clicks to add waypoints
+    const MapClickHandler = () => {
+        useMapEvents({
+            click(e) {
+                const newWaypoint = [e.latlng.lat, e.latlng.lng];
+                setWaypoints([...waypoints, newWaypoint]);
+            },
+        });
+        return null;
     };
 
     return (
         <div className="h-screen w-full">
-            <MapContainer center={[51.505, -0.09]} zoom={5} className="h-full w-full" onClick={handleMapClick}>
-                {/* Map Tiles */}
+            <MapContainer center={[19.0760, 72.8777]} zoom={5} className="h-full w-full">
+                
+                {/* Google Maps Tile Layer in English */}
                 <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                    url="https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}&hl=en"
+                    attribution='&copy; <a href="https://www.google.com/maps">Google Maps</a>'
                 />
+
+                {/* Handle Clicks to Add Waypoints */}
+                <MapClickHandler />
 
                 {/* Markers for Waypoints */}
                 {waypoints.map((position, index) => (
-                    <Marker key={index} position={position}>
+                    <Marker key={index} position={position} draggable={true}>
                         <Popup>Waypoint {index + 1}</Popup>
                     </Marker>
                 ))}
